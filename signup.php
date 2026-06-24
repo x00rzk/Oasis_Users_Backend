148bb8ed60cd1018ebc4895a9fee6411270da71c
x00rzk <kimberly.reyes.itid@unipolidgo.edu.mx>
2026-06-24 08:44:20 -0600

    add cors fix

--- signup.php
+++ signup.php
@@ -1,8 +1,15 @@
 <?php
-header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
+
+header("Content-Type: application/json");
+
 
 require_once "Connection.php";
 
